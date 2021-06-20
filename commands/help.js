d726e5be655e1e5be0008a5b9959c5f3e6e42002
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-20 13:34:26 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -12,7 +12,7 @@ module.exports = {
         .setURL(`https://hyperz.dev/discord`)
         .addFields(
             {name: `What is this?`, value: `${client.user.username} is a Discord bot that will solve all your problems involving time! Have you ever been in a late-night Discord call with your buds, and you don't know what time it is? Well Big Ben here will chime in your channel every 1 hour to help you out!`},
-            {name: `Commands:`, value: `\`ping\` - Ping the bot and check latency.\n\`help\` - Show the help section for the bot.\n\`creator\` - View the creator / coder of this bot.\n\`version\` - View the bots current version.\n\`setchan\` - Sets a channel that the bot will join every hour.`},
+            {name: `Commands:`, value: `\`ping\` - Ping the bot and check latency.\n\`help\` - Show the help section for the bot.\n\`invite\` - Invite the bot to your Discord server.\n\`creator\` - View the creator / coder of this bot.\n\`version\` - View the bots current version.\n\`setchan\` - Sets a channel that the bot will join every hour.\n\`suggest\` - Create a suggestion for Big Benjamin.`},
             {name: `Credits:`, value: `[@aaron5](https://www.tiktok.com/@aaronr5?lang=en) - *Original TikTok Idea.*\n[@Chris](https://twitter.com/groddy12) - *Motivational Support.*\n[@Hyperz](https://hyperz.dev/) - *Physical Programming.*`},
         )
         .setTimestamp()
