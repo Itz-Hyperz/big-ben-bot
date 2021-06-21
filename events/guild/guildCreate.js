dd9684c0171665491bada8f8011c48941f85991a
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-20 20:34:30 -0400

    Update guildCreate.js

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -5,12 +5,18 @@ module.exports = async (client, Hyperz, config, con, guild) =>{
     await con.query(`SELECT * FROM guilds WHERE id='${guild.id}'`, async (err, row) => {
         if(err) throw err;
         if(!row[0]) {
-            await con.query(`INSERT INTO guilds (id, chan) VALUES ('${guild.id}', 'none')`, async (err, row) => {
+            await con.query(`INSERT INTO guilds (id, chan, logs) VALUES ('${guild.id}', 'none', 'none')`, async (err, row) => {
                 if(err) throw err;
             });
         } else if(row[0]) {
             if(row[0].chan != 'none') {
                 await con.query(`UPDATE guilds SET chan='none' WHERE id='${guild.id}'`, async (err, row) => {
+                    if(err) throw err;
+                });
+            }
+
+            if(row[0].logs != 'none') {
+                await con.query(`UPDATE guilds SET logs='none' WHERE id='${guild.id}'`, async (err, row) => {
                     if(err) throw err;
                 });
             }
