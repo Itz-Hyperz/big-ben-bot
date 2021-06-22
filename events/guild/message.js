93866cc4aa4fb2c71dd49458dda3c8d9278bcbfa
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-22 07:54:29 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
 const config = require('../../config.json');
 
-module.exports = (client, Hyperz, config, con, message) =>{
+module.exports = (client, Hyperz, config, con, message) => {
+    
+    if(message.channel.type === 'dm') {
+        return message.channel.send(`You cannot use my DMs, please use a server channel.`).catch(e => {});
+    }
+    
     const prefix = config["main_config"].prefix;
     if(!message.content.startsWith(prefix) || message.author.bot) return;
 
