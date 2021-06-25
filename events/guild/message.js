bb67ce2f0e4dcf1be1374f7d168f3eb49ebe77ab
Itz-Hyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-06-25 00:11:56 -0700

    Update message.js

--- events/guild/message.js
+++ events/guild/message.js
@@ -4,9 +4,33 @@ const config = require('../../config.json');
 module.exports = (client, Hyperz, config, con, message) => {
     
     if(message.author.bot) return;
+
+    const filter = m => m.author.id === message.author.id;
     
     if(message.channel.type === 'dm') {
         return message.channel.send(`You cannot use my DMs, please use a server channel.`).catch(e => {});
+    }
+
+    const insults = ['stupid bot', 'dumb bot', 'gay bot', 'retard', 'retarded bot', 'you suck', 'shit bot', 'bad bot', 'shitter'];
+
+    if(message.mentions.users.first()) {
+        if(message.mentions.users.first().id === client.user.id) {
+
+            insults.forEach(async i => {
+                if(message.content.includes(i)) {
+                    message.channel.send(`This you? https://tenor.com/view/shower-cry-cartoon-network-gif-5184213`).then(() => {
+                        message.channel.awaitMessages(filter, { max: 1, time: 100000, errors: ['time'] })
+                        .then(async collected2 => {
+                            let ress = collected2.first().content.toLowerCase()
+                            if(ress === 'no') {
+                                message.channel.send(`Oh, must have been the wrong one, here, this must be you: https://tenor.com/view/regularshow-gif-5734420`).catch(e => {});
+                            }
+                        }).catch(e => {});
+                    }).catch(e => {});
+                }
+            });
+
+        }
     }
     
     const prefix = config["main_config"].prefix;
