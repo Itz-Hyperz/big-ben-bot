876c61de141b6abb20d4226618671cdc449f6b43
Itz-Hyperz <loganfgross@gmail.com>
2022-07-20 16:19:33 -0700

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const config = require('./config.json');
-const { ShardingManager } = require("discord.js");
-const manager = new ShardingManager("./bot.js", {
-  token: config["main_config"].token
-});
-
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
-manager.spawn();
