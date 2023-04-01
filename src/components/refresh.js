4fa0941ca22a0478dbc695c2ee808888c74612ef
Itz-Hyperz <loganfgross@gmail.com>
2023-04-01 11:26:46 -0700

    Delete src directory

--- src/components/refresh.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function(client, con, interaction, data) {
-    let lol = interaction.message.components;
-    interaction.update({ components: lol }).catch(function(e) { if(client?.config?.debugmode) console.log(e) });
-};
